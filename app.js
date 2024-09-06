036faaeb8db73771f64a76e623895329b79fa76a
Osama Saeed <oosamasaeed893@gmail.com>
2024-09-06 21:25:01 +0500

    Js Assignment 1

---
+++ app.js
@@ -0,0 +1,27 @@
+// / Variables
+
+
+// Bio  data using JS 
+// var firstname = "sabeeh"
+
+
+// var lastname = "Ejaz"
+
+
+// var fullname = "Sabeeh Ejaz"
+
+
+// var city = "Karachi"
+
+
+// var Education = "Undergraduate"
+
+
+// console.log( 
+//     "Hey my name is sabeeh ejaz. I live in karachi. I am an undergraduate right now ")
+
+
+//     console.log( 
+//         "Hey my name is " + fullname + ". I live in " + city + ". I am an " + Education + "right now")
+
+
